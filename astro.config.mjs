f512b198c9ced8aacd4c2c6248e089b39c4342a0
Anup Shinde <anupshinde@gmail.com>
2026-02-03 23:07:31 +0530

    Theme changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://www.pickyfox.com',
   trailingSlash: 'always',
+  integrations: [tailwind()],
   server: {
     host: '0.0.0.0'
   }
