16b0be9faaac44b522085fefb97072d13ab85a83
Anup Shinde <anupshinde@gmail.com>
2026-02-04 00:30:21 +0530

    Styling changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,32 @@ import typography from '@tailwindcss/typography';
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ['"Atkinson Hyperlegible"', 'ui-sans-serif', 'system-ui', 'sans-serif']
+      },
+      colors: {
+        brand: {
+          500: '#ff2f6d'
+        }
+      },
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            fontFamily: theme('fontFamily.sans').join(','),
+            fontSize: '20px',
+            lineHeight: '34px',
+            p: {
+              marginTop: '20px',
+              marginBottom: '20px'
+            },
+            'h1, h2, h3, h4, h5, h6': {
+              fontFamily: theme('fontFamily.sans').join(',')
+            }
+          }
+        }
+      })
+    }
   },
   plugins: [typography]
 };
